5fcbe0fc380a622114dbed9508d748f841426c25
Jennifer S. Bigman <jennifersbigman@gmail.com>
2026-08-05 10:14:41 -0400

    initial commit

---
+++ week-01/exercises/exercises-01.R
@@ -0,0 +1,25 @@
+# Week 1: GitHub workflow practice
+
+# 1. In your own words, explain the difference between:
+#    a. clone and pull
+#    b. commit and push
+
+# Write your answers below.
+
+
+
+# 2. Create two R objects.
+
+student_name <- "replace this with your name"
+favorite_number <- 0
+
+# 3. Use the objects to create a sentence.
+
+introduction <- paste(
+  "My name is",
+  student_name,
+  "and my favorite number is",
+  favorite_number
+)
+
+introduction
